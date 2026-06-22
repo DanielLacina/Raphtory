c9d1a5d83b2ffb521ba9559c59517badd1cc760a
Daniel Lacina <daniellacina8@gmail.com>
2026-06-21 19:54:34 -0500

    working

--- raphtory/src/algorithms/centrality/new_unweighted_pagerank.rs
+++ raphtory/src/algorithms/centrality/new_unweighted_pagerank.rs
@@ -21,18 +21,21 @@ use crate::prelude::NodeFilter;
 struct PageRankState {
     score: f64,
     out_degree: usize,
+    one_degree_out_neighbor_score: f64 
 }
 
 impl PageRankState {
     fn new(num_nodes: usize) -> Self {
         Self {
             score: 1f64 / num_nodes as f64,
+            one_degree_out_neighbor_score: 1f64 / num_nodes as f64,
             out_degree: 0,
         }
     }
 
     fn reset(&mut self) {
         self.score = 0f64;
+        self.one_degree_out_neighbor_score = 0f64;
     }
 }
 
@@ -64,20 +67,21 @@ pub fn unweighted_page_rank<G: StaticGraphViewOps>(
 
     let mut ctx: Context<G, ComputeStateVec> = g.into();
 
-    let mut one_degree_neighbors = HashMap::new();
+    let mut one_degree_neighbors_map = HashMap::new();
 
-    let mut filtered_out_sink_contributor_count = 0;
+    let mut zero_degree_sink_contributor_count = 0;
 
     for node in g.nodes() {
         if node.degree() == 0 {
-            filtered_out_sink_contributor_count += 1;
+            zero_degree_sink_contributor_count += 1;
             continue;
         }
         let one_degree_in_neighbor_count = node.in_neighbours().iter().filter(|n| n.degree() == 1).count();
         let one_degree_out_neighbor_count = node.out_neighbours().iter().filter(|n| n.degree() == 1).count();
-        filtered_out_sink_contributor_count += one_degree_out_neighbor_count; 
-        one_degree_neighbors.insert(node.node, (one_degree_in_neighbor_count, one_degree_out_neighbor_count));
+        one_degree_neighbors_map.insert(node.node, (one_degree_in_neighbor_count, one_degree_out_neighbor_count));
     }
+
+    let one_degree_neighbors = Arc::new(one_degree_neighbors_map);
 
     let subgraph = g.subgraph(g.nodes().select(NodeFilter.degree().gt(1)).unwrap());
 
@@ -87,8 +91,8 @@ pub fn unweighted_page_rank<G: StaticGraphViewOps>(
     let teleport_prob = (1f64 - damp) / n as f64;
     let factor = damp / n as f64;
 
-    let in_degree_zero_score = Arc::new(atomic_float::AtomicF64::new(1.0 / n as f64));
-    let filtered_out_sink_contributor_count = Arc::new(AtomicU64::new(filtered_out_sink_contributor_count as u64));
+    let in_degree_zero_score = Arc::new(atomic_float::AtomicF64::new(0.0));
+    let zero_degree_sink_contributor_count = Arc::new(AtomicU64::new(zero_degree_sink_contributor_count as u64));
 
     let max_diff = accumulators::sum::<f64>(2);
 
@@ -106,6 +110,7 @@ pub fn unweighted_page_rank<G: StaticGraphViewOps>(
     });
 
     let in_degree_zero_score_step2 = Arc::clone(&in_degree_zero_score);
+    let one_degree_neighbors_step2 = Arc::clone(&one_degree_neighbors);
     let step2: ATask<G, ComputeStateVec, PageRankState, _> = ATask::new(move |s| {
         // reset score
         {
@@ -119,7 +124,7 @@ pub fn unweighted_page_rank<G: StaticGraphViewOps>(
             s.get_mut().score += prev.score / prev.out_degree as f64;
         }
 
-        let (one_degree_in_neighbors, _) = one_degree_neighbors.get(&s.node).unwrap_or(&(0, 0));
+        let (one_degree_in_neighbors, _) = one_degree_neighbors_step2.get(&s.node).unwrap_or(&(0, 0));
 
         let in_degree_zero_score = in_degree_zero_score_step2.load(std::sync::atomic::Ordering::Relaxed);
 
@@ -128,10 +133,13 @@ pub fn unweighted_page_rank<G: StaticGraphViewOps>(
         s.get_mut().score *= damp;
 
         s.get_mut().score += teleport_prob;
+
+        let one_degree_out_neighbor_score = (s.prev().score / s.prev().out_degree as f64 * damp) + teleport_prob; 
+        s.get_mut().one_degree_out_neighbor_score = one_degree_out_neighbor_score;
         Step::Continue
     });
 
-
+    let one_degree_neighbors_step3 = Arc::clone(&one_degree_neighbors);
     let step3 = ATask::new(move |s| {
         let state: &mut PageRankState = s.get_mut();
 
@@ -140,47 +148,65 @@ pub fn unweighted_page_rank<G: StaticGraphViewOps>(
 
             let ts_contrib = factor * curr;
             s.global_update(&total_sink_contribution, ts_contrib);
+        } else {
+            let (_, one_degree_out_neighbors) = one_degree_neighbors_step3.get(&s.node).unwrap_or(&(0, 0));
+            let one_degree_out_neighbor_score = s.prev().one_degree_out_neighbor_score; 
+            let contrib = (*one_degree_out_neighbors as f64) * factor * one_degree_out_neighbor_score;
+            s.global_update(&total_sink_contribution, contrib);
         }
+
         Step::Continue
     });
 
-    let filtered_out_sink_contributor_count_step4 = Arc::clone(&filtered_out_sink_contributor_count);
+    let zero_degree_sink_contributor_count_step4 = Arc::clone(&zero_degree_sink_contributor_count);
     let in_degree_zero_score_step4 = Arc::clone(&in_degree_zero_score);
     let step4 = ATask::new(move |s| {
         //read total sink contribution
         let total_sink_contribution = s
             .read_global_state(&total_sink_contribution)
             .unwrap_or_default();
-        let filtered_out_contributor_count = filtered_out_sink_contributor_count_step4.load(std::sync::atomic::Ordering::Relaxed) as f64;
+        let filtered_out_contributor_count = zero_degree_sink_contributor_count_step4.load(std::sync::atomic::Ordering::Relaxed) as f64;
         let in_degree_zero_score = in_degree_zero_score_step4.load(std::sync::atomic::Ordering::Relaxed);
-        // update local score with total sink contribution
-        let state: &mut PageRankState = s.get_mut();
-        state.score += total_sink_contribution + filtered_out_contributor_count * factor * in_degree_zero_score;
+        let (curr_score, curr_one_degree_out_neighbor_score) = {
+            // update local score with total sink contribution
+            let state: &mut PageRankState = s.get_mut();
+            state.score += total_sink_contribution + filtered_out_contributor_count * factor * in_degree_zero_score;
+            state.one_degree_out_neighbor_score += total_sink_contribution + filtered_out_contributor_count * factor * in_degree_zero_score;
+
+            (state.score, state.one_degree_out_neighbor_score)
+        };
 
         // update global max diff
-
-        let curr = state.score;
-        let prev = s.prev().score;
-
+        let prev_score = s.prev().score;
         let md = if use_l2_norm {
-            f64::powi(abs(prev - curr), 2)
+            f64::powi(abs(prev_score - curr_score), 2)
         } else {
-            abs(prev - curr)
+            abs(prev_score - curr_score)
         };
 
         s.global_update(&max_diff, md);
+
+        let prev_one_degree_out_neighbor_score = s.prev().one_degree_out_neighbor_score;
+        let md = if use_l2_norm {
+            f64::powi(abs(prev_one_degree_out_neighbor_score - curr_one_degree_out_neighbor_score), 2)
+        } else {
+            abs(prev_one_degree_out_neighbor_score - curr_one_degree_out_neighbor_score)
+        };
+
+        s.global_update(&max_diff, md);
+
         Step::Continue
     });
 
 
-    let filtered_out_sink_contributor_count_step5 = Arc::clone(&filtered_out_sink_contributor_count);
-    let in_degree_zero_score_step5 = Arc::clone(&in_degree_zero_score);
+    let zero_degree_sink_contributor_count_step5 = Arc::clone(&zero_degree_sink_contributor_count);
+    let prev_in_degree_zero_score_step5 = Arc::clone(&in_degree_zero_score);
     let step5 = Job::Check(Box::new(move |state| {
         let total_sink_contribution = state.read(&total_sink_contribution);
-        let filtered_out_sink_contributor_count = filtered_out_sink_contributor_count_step5.load(std::sync::atomic::Ordering::Relaxed) as f64;
-        let prev_in_degree_zero_score = in_degree_zero_score_step5.load(std::sync::atomic::Ordering::Relaxed);
-        let cur_in_degree_zero_score = (prev_in_degree_zero_score * damp + teleport_prob) + total_sink_contribution + filtered_out_sink_contributor_count * factor * prev_in_degree_zero_score;
-        in_degree_zero_score_step5.store(cur_in_degree_zero_score, std::sync::atomic::Ordering::Relaxed);
+        let zero_degree_sink_contributor_count = zero_degree_sink_contributor_count_step5.load(std::sync::atomic::Ordering::Relaxed) as f64;
+        let prev_in_degree_zero_score = prev_in_degree_zero_score_step5.load(std::sync::atomic::Ordering::Relaxed);
+        let cur_in_degree_zero_score = teleport_prob + total_sink_contribution + zero_degree_sink_contributor_count * factor * prev_in_degree_zero_score;
+        prev_in_degree_zero_score_step5.store(cur_in_degree_zero_score, std::sync::atomic::Ordering::Relaxed);
         let diff_in_degree_zero_score = abs(prev_in_degree_zero_score - cur_in_degree_zero_score);
         let max_diff_val = state.read(&max_diff).max(diff_in_degree_zero_score);
         let cont = if use_l2_norm {
